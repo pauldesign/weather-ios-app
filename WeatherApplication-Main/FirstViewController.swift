bf7c6164a49a1a6eaca40944a6325175714663a8
Paul Maxeiner <paulmaxeiner@pauls-macbook-air.local>
2016-08-02 12:00:30 -0400

    Initial Commit

---
+++ WeatherApplication-Main/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WeatherApplication-Main
+//
+//  Created by Paul Maxeiner on 8/2/16.
+//  Copyright © 2016 Paul Maxeiner. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WeatherApplication-Main/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WeatherApplication-Main
+//
+//  Created by Paul Maxeiner on 8/2/16.
+//  Copyright © 2016 Paul Maxeiner. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
